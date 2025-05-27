c997f2990b448fe8265411fdf3e151d90728cef6
Bryan . <bryangod194@gmail.com>
2025-05-27 16:04:10 -0400

    Api arreglada para consumo en frontend (usar where)

--- app/Http/Controllers/TuristaController.php
+++ app/Http/Controllers/TuristaController.php
@@ -10,27 +10,26 @@ class TuristaController extends Controller
         $turistas = Turista::all();
         return response()->json($turistas);
     }
-    public function selectId($mail){
-        $turistas = Turista::all()->where("correo_electronico", '=', $mail);
-        return response()->json($turistas);
+    public function selectMail($mail){
+        $turistas = Turista::where('correo_electronico', '=',$mail)->get();
+        return response()->json($turistas->all());
     }
     public function registrar(Request $request){
         try{
             $turista = Turista::create([
-            'correo_electronico' => $request->input('correo_electronico'),
-            'contrasenia' => $request->input('contrasenia'),
-            'documento' => $request->input('documento'),
-            'nombre' => $request->input('nombre'),
-            'ap_pat' => $request->input('ap_pat'),
-            'ap_mat' => $request->input('ap_mat'),
-            'fecha_nac' => $request->input('fecha_nac'),
-            'nacionalidad' => $request->input('nacionalidad'),
-            'prefijo_telefonico' => $request->input('prefijo_telefonico'),
-            'telefono' => $request->input('telefono')
+            'correo_electronico' => $request->correo_electronico,
+            'contrasenia' => $request->contrasenia,
+            'documento' => $request->documento,
+            'nombre' => $request->nombre,
+            'ap_pat' => $request->ap_pat,
+            'ap_mat' => $request->ap_mat,
+            'fecha_nac' => $request->fecha_nac,
+            'nacionalidad' => $request->nacionalidad,
+            'prefijo_telefonico' => $request->prefijo_telefonico,
+            'telefono' => $request->telefono
         ]);
 
         return response()->json($turista, 201);
-        
         }catch (\Exception $e) {
             return response()->json(['error' => 'Error al registrar el turista: ' . $e->getMessage()], 500);
         }
