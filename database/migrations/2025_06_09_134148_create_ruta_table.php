8ca49ee45b7922411504299a9d95e8a71e85b918
Carlos Conde <carloscondeeduardomamani@gmail.com>
2025-06-09 10:25:49 -0400

    agregado reserva ruta sitio tour y sitiocategoria migracion

---
+++ database/migrations/2025_06_09_134148_create_ruta_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    
+    public function up(): void
+    {
+        Schema::create('ruta', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_tour');
+            $table->unsignedBigInteger('id_sitio');
+            $table->integer('orden');
+            $table->timestamps();
+
+            $table->primary(['id_tour', 'id_sitio']);
+
+            $table->foreign('id_tour')
+                  ->references('id_tour')
+                  ->on('tour')
+                  ->onDelete('cascade');
+
+            $table->foreign('id_sitio')
+                  ->references('id_sitio')
+                  ->on('sitio')
+                  ->onDelete('cascade');
+        });
+    }
+
+    
+    public function down(): void
+    {
+        Schema::dropIfExists('ruta');
+    }
+};
