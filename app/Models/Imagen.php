01dbf5285ac0f1405bfa527581ba9487e49b9665
Angel Fabricio Paredes Campos <pca2029623@est.univalle.edu>
2025-06-09 11:59:30 -0400

    imagen

--- app/Models/Imagen.php
+++ app/Models/Imagen.php
@@ -8,4 +8,9 @@ class Imagen extends Model
 {
     protected $table = 'Imagen';
 protected $fillable = ['id_sitio', 'url_img'];
+ public function sitio()
+    {
+        return $this->belongsTo(Sitio::class, 'id_sitio');
+    }
 }
+
